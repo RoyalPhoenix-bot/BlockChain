0aa3ef9bb1355cb899bb34a5a4108e3e3055ceb5
Kushagra Sharma <kushagrasharma.sharma@gmail.com>
2021-07-20 18:39:15 +0530

    Add Assignment6, browser trial

---
+++ Assignment6/Trial code from Browser/web-trial.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var crypto = require('crypto');
+const { response } = require('express');
+const app = require("express")();
+//var bodyParser = require('body-parser');
+app.use(express.urlencoded({extended : false}));
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.send("Never gonna give you up, Never gonna let you down, Never gonna run around and desert you, Never gonna make you cry, Never gonna say goodbye, Never gonna tell a lie and hurt you.");
+})
+
+app.get("/hash", (req,res)=>{
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post("/hash",function(req,res){
+    console.log("Data is: ",JSON.stringify(req.body));
+    var hash = crypto.createHash('sha256').update(req.body.data).digest('hex').toString();
+    res.json({'hash':hash})
+    //res.send(JSON.stringify({"hash":hash}),'\n');
+    console.log(JSON.stringify({"hash":hash}),'\n');
+})
+app.listen(8787, ()=>console.log("If I'm printed, the Local Node is Active.\n"));
